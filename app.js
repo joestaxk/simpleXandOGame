14210fa394d05e0913ee2db9aa37b226a1df2492
joestaxk <joestaxk@gmail.com>
2022-05-09 06:46:12 -0400

    pushing final application

--- app.js
+++ app.js
@@ -9,4 +9,11 @@ window.addEventListener("beforeunload", beforeUnloadListener, {once:true})
 const intro_board = document.querySelector("#intro")
 const intro =  new XOIntro(intro_board)
 
+// detect mobile 
+const isMobile = navigator.userAgentData.mobile;
+
+if(isMobile) {
+    alert("This application wasn't meant for mobile, mobile feature coming soon!!")
+}
+
 intro.initiate()
